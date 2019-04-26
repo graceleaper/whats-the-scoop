8fe8abb64e3672fbd41223b4e29540fafa0cedcf
Grace Lee <igracelee1@gmail.com>
2019-04-26 17:22:51 -0400

    SingleArticle.js cleanup

--- src/SingleArticle.js
+++ src/SingleArticle.js
@@ -33,29 +33,3 @@ export default class SingleArticle extends React.Component {
       );
   }  
 }
-
-// import React from "react";
-// import "./App.css";
-// // import RevealCard from "./RevealCard"
-
-// export default class SingleArticle extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-
-//     }
-//   }
-//   render() {
-//     return (
-//           <div className="article-card">
-//           <h1>{this.props.title}</h1>
-//           {/* <img alt='new image here' src={props.urlToImage} /> */}
-//           <p>{this.props.content}</p>
-//           <img className="article-image" alt="new img here" src={this.props.urlToImage} />
-//           <p>{this.props.url}</p>
-//           <button className="share-button">Share</button>
-//           <button className="fact-check-button">Fact-Check</button>
-//         </div>
-//       );
-//   }  
-// }

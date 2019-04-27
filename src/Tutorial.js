eeff380af460927205599b2d548170e602d8ca27
Grace Lee <igracelee1@gmail.com>
2019-04-27 19:54:56 -0400

    images for tutorial and quiz

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -9,7 +9,7 @@ function Tutorial() {
       <header className="App-header">
         <h1 className="logo">What's the Scoop?</h1>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img className="responsive_gif" alt="logo here" src={require('./animat-responsive-color.gif')} />
+        <img className="responsive_gif" alt="logo here" src={require('./animat-search-color.gif')} />
         <h2>Tutorial</h2>
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
